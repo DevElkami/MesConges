2ec2090324cebffc8f2294f764da1c91b759bae6
Elkami <49388297+DevElkami@users.noreply.github.com>
2022-10-05 14:33:08 +0200

    .NET5

--- src/Program.cs
+++ src/Program.cs
@@ -9,11 +9,8 @@ namespace WebApplicationConges
         public static void Main(string[] args)
         {
             Toolkit.InitConfiguration();
-#if DEBUG
-            Db.Instance.Init("Server=localhost;Port=3306;Uid=root;Pwd=acxbn45;Character Set=utf8;", Toolkit.Configuration[Toolkit.ConfigEnum.DbName.ToString()]);
-#else
             Db.Instance.Init(Toolkit.Configuration[Toolkit.ConfigEnum.ConnectionString.ToString()], Toolkit.Configuration[Toolkit.ConfigEnum.DbName.ToString()]);
-#endif
+
             BuildWebHost(args).Run();
         }
 
