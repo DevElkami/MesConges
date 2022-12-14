a630d8178259660220feec151c331c2d12890317
DevElkami <m.reignier@elkami.fr>
2022-12-14 08:37:31 +0100

    Enable custom display for days off

---
+++ src/Code/Model/Custom.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationConges.Model
+{
+    public class Custom
+    {
+        public bool DisplayPublicCalendar { get; set; } = true;
+        public bool DisplayCongeSansSolde { get; set; } = true;
+        public bool DisplayCongeRecup { get; set; } = true;
+        public bool DisplayCongeAbsTmp { get; set; } = true;
+        public bool DisplayCongeFamilyEvent { get; set; } = true;
+        public bool DisplayCongeChildSick { get; set; } = true;
+        public bool DisplayCongeInformation { get; set; } = true;
+    }
+}
